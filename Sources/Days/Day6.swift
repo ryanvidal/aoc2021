4684c96586dea66ea8c23c09cede011c9fed2561
Ryan Vidal <rvidal@squareup.com>
2021-12-06 17:09:08 -0500

    Solve Day6 and add convenience functions

--- Sources/Days/Day6.swift
+++ Sources/Days/Day6.swift
@@ -15,27 +15,79 @@ public class Day6Solver: DailySolver {
     public typealias CalculationInput = [Int]
 
     public func ParseInput(_ input: String) -> CalculationInput {
-        let inputLines = input.components(separatedBy: "\n")
-        return inputLines.compactMap { Int($0) }
+        let inputLines = input.components(separatedBy: ",")
+        return inputLines.compactMap { Int($0)! }
     }
 
     public func PerformPart1Calculation(_ input: CalculationInput) -> Double? {
-        
-        // Part 1 calculation here
-
-        return nil
+        return input.reduce(0.0) { $0 + numberOfFish(after: 80, daysToDelay: $1) }
     }
 
     public func PerformPart2Calculation(_ input: CalculationInput) -> Double? {
-        
-        // Part 2 calculation here
-
-        return nil
+        return input.reduce(0.0) { $0 + numberOfFish(after: 256, daysToDelay: $1) }
     }
 }
 
+fileprivate var cache: [Pair:Double] = [:]
+
+private func numberOfFish(after numDays: Int, daysToDelay: Int) -> Double {
+    if let cachedValue = cache[Pair(numDays, daysToDelay)] { return cachedValue }
+
+    guard numDays >= daysToDelay else { return 1.0 }
+
+    var finalCount = 1.0
+
+    let spawns = (numDays - daysToDelay + 6) / 7
+
+    for spawn in 0..<spawns {
+        let daysLeftAfterSpawn = numDays - daysToDelay - spawn * 7
+        finalCount += numberOfFish(after: daysLeftAfterSpawn, daysToDelay: 9)
+    }
+
+    cache[Pair(numDays, daysToDelay)] = finalCount
+
+    return finalCount
+}
+
+// Naive approach:
+
+//        var school = input
+//        for _ in 1...80 {
+//            var newLanternFish = [LanternFish]()
+//            school.forEach { fish in
+//                if let newFish = fish.shouldSpawn() {
+//                    newLanternFish.append(newFish)
+//                }
+//            }
+//            school.append(contentsOf: newLanternFish)
+//        }
+//
+//        return Double(school.count)
+
+//public class LanternFish {
+//    static let gestationStart = 6
+//
+//    var spawnTimer: Int
+//
+//    init(_ initialTimer: Int = 8) {
+//        self.spawnTimer = initialTimer
+//    }
+//
+//    public func shouldSpawn() -> LanternFish? {
+//        spawnTimer -= 1
+//        if spawnTimer < 0 {
+//            spawnTimer = LanternFish.gestationStart
+//            return LanternFish()
+//        }
+//
+//        return nil
+//    }
+//}
+
 fileprivate let testInputString = """
+3,4,3,1,2
 """
 
 fileprivate let puzzleInputString = """
+5,1,1,3,1,1,5,1,2,1,5,2,5,1,1,1,4,1,1,5,1,1,4,1,1,1,3,5,1,1,1,1,1,1,1,1,1,4,4,4,1,1,1,1,1,4,1,1,1,1,1,5,1,1,1,4,1,1,1,1,1,3,1,1,4,1,4,1,1,2,3,1,1,1,1,4,1,2,2,1,1,1,1,1,1,3,1,1,1,1,1,2,1,1,1,1,1,1,1,4,4,1,4,2,1,1,1,1,1,4,3,1,1,1,1,2,1,1,1,2,1,1,3,1,1,1,2,1,1,1,3,1,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,3,1,1,1,1,1,1,2,1,1,2,3,1,2,1,1,4,1,1,5,3,1,1,1,2,4,1,1,2,4,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,4,3,1,2,1,2,1,5,1,2,1,1,5,1,1,1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,1,1,1,1,1,3,1,1,5,1,1,1,1,5,1,4,1,1,1,4,1,3,4,1,4,1,1,1,1,1,1,1,1,1,3,5,1,3,1,1,1,1,4,1,5,3,1,1,1,1,1,5,1,1,1,2,2
 """
